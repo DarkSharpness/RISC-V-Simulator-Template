68dd99a8268c155012f331d2d3c38537bd82fb6a
DarkSharpness <2040703891@qq.com>
2024-07-09 21:04:36 +0800

    initial commit | complete a draft outline

---
+++ include/debug.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <utility>
+#include <iostream>
+#include <source_location>
+
+namespace dark::debug {
+
+/**
+ * Copied from https://en.cppreference.com/w/cpp/utility/unreachable 
+ * If compiled under C++23, just use std::unreachable() instead.
+*/
+[[noreturn]] inline void unreachable() {
+#if __cplusplus > 202002L
+    std::unreachable();
+#elif defined(_MSC_VER) && !defined(__clang__) // MSVC
+    // Uses compiler specific extensions if possible.
+    // Even if no extension is used, undefined behavior is still raised by
+    // an empty function body and the noreturn attribute.
+    __assume(false);
+#else // GCC, Clang
+    __builtin_unreachable();
+#endif
+}
+
+template <typename _Tp, typename... _Args>
+struct assert {
+    explicit assert(_Tp &&condition, _Args &&...args,
+        std::source_location location = std::source_location::current()) {
+        if (condition) return;
+        std::cerr << "Assertion failed at: "
+            << location.file_name() << ":" << location.line() << "\n";
+        if constexpr (sizeof...(args) != 0) {
+            std::cerr << "Message: ";
+            ((std::cerr << args), ...) << std::endl;
+        }
+        std::exit(EXIT_FAILURE);
+    }
+};
+
+template <typename _Tp, typename... _Args>
+assert(_Tp &&, _Args &&...) -> assert<_Tp, _Args...>;
+
+template <typename _Tp, _Tp _Default>
+struct DebugValue {
+#ifdef _DEBUG
+  private:
+    _Tp _M_value = _Default;
+  public:
+    auto get_value() const { return this->_M_value; }
+    auto set_value(_Tp value) { this->_M_value = value; }
+#else
+  public:
+    auto get_value() const { return _Default; }
+    auto set_value(_Tp) { /* do nothing */ }
+#endif
+  public:
+    explicit operator _Tp() const { return this->get_value(); }
+    DebugValue &operator=(_Tp value) { this->set_value(value); return *this; }
+};
+
+} // namespace dark::debug
---
+++ include/target.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <cstdint>
+
+namespace dark {
+
+using target_size_t = std::uint32_t;
+using target_ssize_t = std::int32_t;
+
+} // namespace dark
